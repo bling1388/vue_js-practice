4a1b3915bb2a8f2b2b839f0de22552ed9bca05db
bling1388 <81565104+bling1388@users.noreply.github.com>
2022-11-19 10:21:06 +0100

    fix vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,5 @@ import './bootstrap';
 import  {createApp} from 'vue';
 import app from './components/app.vue';
 
+
 createApp(app).mount("#app")
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vutejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'
+
 
 export default defineConfig({
     plugins: [
